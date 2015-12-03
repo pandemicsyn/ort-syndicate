e899b213824fedfab94c0400d6c46001662b9aa8
pandemicsyn <syn@ronin.io>
2015-12-03 18:04:19 +0000

    Add facilities for clean shutdown

--- synd/main.go
+++ synd/main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"os/signal"
 	"sync"
+	"syscall"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
@@ -37,13 +39,18 @@ type RingSyndicate struct {
 	sync.RWMutex
 	active bool
 	name   string
-	config *syndicate.Config
+	config syndicate.Config
 	server *syndicate.Server
+	gs     *grpc.Server
 }
 
 type RingSyndicates struct {
 	sync.RWMutex
-	Syndics []*RingSyndicate
+	Syndics          []*RingSyndicate
+	ch               chan bool //os signal chan,
+	ShutdownComplete chan bool
+	waitGroup        *sync.WaitGroup
+	stopped          bool
 }
 
 type ClusterConfigs struct {
@@ -51,36 +58,44 @@ type ClusterConfigs struct {
 	GroupSyndicate *syndicate.Config
 }
 
-func launchSyndicates(rs *RingSyndicates) {
-	rs.Lock()
-	defer rs.Unlock()
-	for k, _ := range rs.Syndics {
-		rs.Syndics[k].Lock()
-		l, err := net.Listen("tcp", fmt.Sprintf(":%d", rs.Syndics[k].config.Port))
-		if err != nil {
-			log.Fatalln(err)
-			return
-		}
-		var opts []grpc.ServerOption
-		creds, err := credentials.NewServerTLSFromFile(rs.Syndics[k].config.CertFile, rs.Syndics[k].config.KeyFile)
-		if err != nil {
-			log.Fatalln("Error load cert or key:", err)
-		}
-		opts = []grpc.ServerOption{grpc.Creds(creds)}
-		s := grpc.NewServer(opts...)
-
-		if rs.Syndics[k].config.Master {
-			pb.RegisterSyndicateServer(s, rs.Syndics[k].server)
-			log.Println("Master starting up on", rs.Syndics[k].config.Port)
-			s.Serve(l)
-		} else {
-			//pb.RegisterRingDistServer(s, newRingDistServer())
-			//log.Printf("Starting ring slave up on %d...\n", cfg.Port)
-			//s.Serve(l)
-			log.Fatalln("Syndicate slaves not implemented yet")
-		}
-		rs.Syndics[k].Unlock()
+func (rs *RingSyndicates) Stop() {
+	log.Println("Exiting...")
+	close(rs.ch)
+	for i, _ := range rs.Syndics {
+		rs.Syndics[i].gs.Stop()
 	}
+	rs.waitGroup.Wait()
+	close(rs.ShutdownComplete)
+}
+
+func (rs *RingSyndicates) launchSyndicates(k int) {
+	rs.Syndics[k].Lock()
+	defer rs.waitGroup.Done()
+	rs.waitGroup.Add(1)
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", rs.Syndics[k].config.Port))
+	if err != nil {
+		log.Fatalln(err)
+		return
+	}
+	var opts []grpc.ServerOption
+	creds, err := credentials.NewServerTLSFromFile(rs.Syndics[k].config.CertFile, rs.Syndics[k].config.KeyFile)
+	if err != nil {
+		log.Fatalln("Error load cert or key:", err)
+	}
+	opts = []grpc.ServerOption{grpc.Creds(creds)}
+	rs.Syndics[k].gs = grpc.NewServer(opts...)
+
+	if rs.Syndics[k].config.Master {
+		pb.RegisterSyndicateServer(rs.Syndics[k].gs, rs.Syndics[k].server)
+		log.Println("Master starting up on", rs.Syndics[k].config.Port)
+		rs.Syndics[k].gs.Serve(l)
+	} else {
+		//pb.RegisterRingDistServer(s, newRingDistServer())
+		//log.Printf("Starting ring slave up on %d...\n", cfg.Port)
+		//s.Serve(l)
+		log.Fatalln("Syndicate slaves not implemented yet")
+	}
+	rs.Syndics[k].Unlock()
 }
 
 func main() {
@@ -97,7 +112,12 @@ func main() {
 		fmt.Println("go version:", goVersion)
 		return
 	}
-	rs := &RingSyndicates{}
+	rs := &RingSyndicates{
+		ch:               make(chan bool),
+		ShutdownComplete: make(chan bool),
+		waitGroup:        &sync.WaitGroup{},
+		stopped:          false,
+	}
 
 	var tc map[string]syndicate.Config
 	if _, err := toml.DecodeFile(configFile, &tc); err != nil {
@@ -109,7 +129,7 @@ func main() {
 		syndic := &RingSyndicate{
 			active: false,
 			name:   k,
-			config: &v,
+			config: v,
 		}
 		syndic.server, err = syndicate.NewServer(&v, k)
 		if err != nil {
@@ -117,5 +137,21 @@ func main() {
 		}
 		rs.Syndics = append(rs.Syndics, syndic)
 	}
-	launchSyndicates(rs)
+	rs.Lock()
+	defer rs.Unlock()
+	for k, _ := range rs.Syndics {
+		go rs.launchSyndicates(k)
+	}
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT, syscall.SIGTERM)
+	for {
+		select {
+		case <-ch:
+			rs.Stop()
+			<-rs.ShutdownComplete
+			return
+		case <-rs.ShutdownComplete:
+			return
+		}
+	}
 }
